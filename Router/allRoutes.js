b9671e3c17c369ccf72dfeb5bd89afa3e09f8d1c
myriam <myriam-iria@hotmail.fr>
2025-01-10 20:01:03 +0100

    page accueil

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,7 +2,9 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
+    new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/liste-event", "Liste Event", "/pages/liste-event.html"),
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Esportify";
