d57662c71255b0bba5be0920618f4bbc8d40fc42
myriam <myriam-iria@hotmail.fr>
2025-01-14 13:16:10 +0100

    pages account

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,10 +3,12 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/liste-event", "Liste event", "/pages/liste-event.html"),
+    new Route("/liste-event", "Liste event", "/pages/event/liste-event.html"),
     new Route("/signin", "Connexion", "/pages/auth/signin.html"),
     new Route("/signup", "Inscription", "/pages/auth/signup.html"),
-    new Route("/myaccount", "Gestion du compte", "/pages/auth/myaccount.html"),
+    new Route("/myaccount", "Gestion du compte", "/pages/account/myaccount.html"),
+    new Route("/accounts", "Gestion des comptes", "/pages/account/accounts.html"),
+    new Route("/user-account", "Gestion d'un comptes", "/pages/account/user-account.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
