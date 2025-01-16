432781b976d211ab3fc1f826022db7ecbd254c6c
myriam <myriam-iria@hotmail.fr>
2025-01-16 17:10:48 +0100

    page avis

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -12,6 +12,7 @@ export const allRoutes = [
     new Route("/scores", "Scores", "/pages/scores.html"),
     new Route("/liste-participant", "Liste participant", "/pages/event/liste-participant.html"),
     new Route("/contact", "Contact", "/pages/contact.html"),
+    new Route("/avis", "Avis", "/pages/avis/avis.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
