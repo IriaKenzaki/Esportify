54143a6f8c14f06b9ba868bda350c900041b71e5
myriam <myriam-iria@hotmail.fr>
2025-01-16 13:23:24 +0100

    page liste participant

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -10,6 +10,7 @@ export const allRoutes = [
     new Route("/accounts", "Gestion des comptes", "/pages/account/accounts.html"),
     new Route("/useraccount", "Gestion d'un compte", "/pages/account/useraccount.html"),
     new Route("/scores", "Scores", "/pages/scores.html"),
+    new Route("/liste-participant", "Liste participant", "/pages/event/liste-participant.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
