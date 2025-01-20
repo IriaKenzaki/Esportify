d4b002831ae6ed081788edcc24e13f31f4d02114
myriam <myriam-iria@hotmail.fr>
2025-01-20 16:01:11 +0100

    page inscriptions

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -12,6 +12,7 @@ export const allRoutes = [
     new Route("/scores", "Scores", "/pages/scores.html"),
     new Route("/liste-participant", "Liste participant", "/pages/event/liste-participant.html"),
     new Route("/new-event", "Création d'évènement", "/pages/event/new-event.html"),
+    new Route("/inscription", "Mes inscriptions", "/pages/event/inscription.html"),
     new Route("/contact", "Contact", "/pages/contact.html"),
     new Route("/avis", "Avis", "/pages/avis/avis.html"),
 ];
